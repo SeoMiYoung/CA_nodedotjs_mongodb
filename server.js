374b9e92e83f3f5e127e6ed9f3eda2852ae98e0a
SeoMiYoung <seo20seo00@naver.com>
2024-06-21 21:38:57 +0900

    데이터베이스에 데이터 저장하기

--- server.js
+++ server.js
@@ -18,14 +18,28 @@ const { MongoClient } = require('mongodb')
 
 let db
 const url = DB_URL
-new MongoClient(url).connect().then((client)=>{
+new MongoClient(url).connect().then((client)=>{ // 몽고디비 접속
   console.log('DB연결성공')
-  db = client.db('forum')
+  db = client.db('forum') // forum이라는 데이터베이스에 연결
 
   app.listen(PORT, () => {
     console.log('http://localhost:'+ PORT +'에서 서버 실행중')
   })
 
-}).catch((err)=>{
+}).catch((err)=>{ // 에러나면 이거 띄우기
   console.log(err)
+})
+
+
+app.get('/', (요청, 응답) => {
+    응답.send('기본 페이지에요~')
+})
+
+// 누군가 /shop에 접속하면 DB에 뭔가 저장해보자
+app.get('/shop', (요청, 응답) => {
+    db.collection('shopData').insertOne({ // forum의 shopData에 저장됨
+        name: 'seo',
+        item: 'shirts',
+        num: 1
+    })
 })
